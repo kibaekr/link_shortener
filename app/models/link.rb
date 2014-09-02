e13bde8713be48d1b1ac019a4bce212bc009eb91
Keith Ryu <kibaekr@gmail.com>
2014-09-02 00:05:03 -0700

    addded extra uniqueness to short_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,8 +5,9 @@ class Link < ActiveRecord::Base
 
   before_save :shortenize_link
   validates :original_link, url: true, presence: true
+  validates :short_link, uniqueness: true
 
   def shortenize_link
-    self.short_link = Digest::MD5.hexdigest(original_link)[0..5]
+    self.short_link = Digest::MD5.hexdigest(original_link)[0..5]<< Link.count.to_s #append uniq digits to differentiate links with same original link
   end
 end
