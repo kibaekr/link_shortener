b8b36359370a2267903ecf6179918bccd811a392
Keith Ryu <kibaekr@gmail.com>
2014-09-01 22:09:14 -0700

    validate original_link url format

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,8 @@ class Link < ActiveRecord::Base
   has_many :link_hits
 
   before_save :shortenize_link
-  validates_presence_of :original_link
+  # validates_presence_of :original_link
+  validates :original_link, url: true, presence: true
 
   def shortenize_link
     self.short_link = Digest::MD5.hexdigest(original_link)[0..5]
